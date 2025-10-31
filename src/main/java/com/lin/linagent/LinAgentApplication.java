863dbfce76c3ce34719f66c6c4df8760872b3eaa
zhanglinshuai <1216486490@qq.com>
2025-10-31 18:59:00 +0800

    初始化项目

---
+++ src/main/java/com/lin/linagent/LinAgentApplication.java
@@ -0,0 +1,13 @@
+package com.lin.linagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinAgentApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lin/linagent/contant/CommonVariables.java
@@ -0,0 +1,12 @@
+package com.lin.linagent.contant;
+
+/**
+ * 常用的变量
+ * @author zhanglinshuai
+ */
+public interface CommonVariables {
+    /**
+     * 阿里云百炼平台的apikey
+     */
+     String API_KEY = "sk-6dc2d723f04249d4b8f203d8e43fdaa3";
+}
