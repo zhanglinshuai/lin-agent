b1fc3a4bc7eecadf70e94549b90bbfc55499173e
zhanglinshuai <1216486490@qq.com>
2025-10-31 21:19:46 +0800

    LangChain4j调用，SpringAi调用灵积大模型

---
+++ src/main/java/com/lin/linagent/invoke/LangChainInvokeAgent.java
@@ -0,0 +1,18 @@
+package com.lin.linagent.invoke;
+
+import com.lin.linagent.contant.CommonVariables;
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+
+/**
+ * 使用LangChain4j调用灵积大模型
+ */
+public class LangChainInvokeAgent {
+    public static void main(String[] args) {
+        QwenChatModel chatModel = QwenChatModel.builder()
+                .apiKey(CommonVariables.API_KEY)
+                .modelName("qwen-max")
+                .build();
+        String answer = chatModel.chat("你擅长干什么？");
+        System.out.println(answer);
+    }
+}
---
+++ src/main/java/com/lin/linagent/invoke/SpringAIInvokeAgent.java
@@ -0,0 +1,27 @@
+package com.lin.linagent.invoke;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * 使用spring ai alibaba调用灵积大模型
+ *  通过ChatModel对象调用大模型，适合简单的对话场景
+ * @author zhanglinshuai
+ */
+@Component
+public class SpringAIInvokeAgent implements CommandLineRunner {
+    @Resource
+    private ChatModel dashscopeChatModel;
+
+    @Override
+    public void run(String... args) throws Exception {
+        AssistantMessage assistantMessage = dashscopeChatModel.call(new Prompt("你好，你会做什么?"))
+                .getResult()
+                .getOutput();
+        System.out.println(assistantMessage.getText());
+    }
+}
